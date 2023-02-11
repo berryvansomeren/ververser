ae0a65e324f296872cc6fe5b94198eb34f6d444a
Berry <berryvansomeren@gmail.com>
2023-02-11 22:53:17 +0100

    Updated the example game

--- example/content/main.py
+++ example/content/main.py
@@ -3,18 +3,30 @@ from game_window import GameWindow
 from pyglet.gl import glClearColor
 
 
-_GAME : Optional[ GameWindow ] = None
+class Game:
 
+    def __init__( self, game_window : GameWindow ):
+        self.game_window = game_window
+        glClearColor( 255 / 255, 10 / 255, 10 / 255, 1.0 )  # red, green, blue, and alpha(transparency)
+
+    def update( self, dt ):
+        ...
+
+    def draw( self ):
+        ...
+
+
+# --------
+# This is just some boilerplate that makes it easier to use a custom Game class
+
+_GAME : Optional[ Game ] = None
 
 def init( game_window : GameWindow ):
     global _GAME
-    _GAME = game_window
-
-    glClearColor( 255/255, 10/255, 10/255, 1.0 )  # red, green, blue, and alpha(transparency)
-
+    _GAME = Game( game_window )
 
 def update( dt ):
-    ...
+    _GAME.update( dt )
 
 def draw():
-    ...
+    _GAME.draw()
